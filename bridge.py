8da1c501edc35f0f8b889b02acbb78a566c66d68
Wanderrer <rwboshae@gmail.com>
2018-03-08 10:38:22 -0800

    Modified test

--- bridge.py
+++ bridge.py
@@ -7,13 +7,12 @@ import commands         # Used to get Local IP and MAC informtation
 
 ips = commands.getoutput("/sbin/ifconfig | grep -i \"inet\" | awk '{print $2}'")
 ips = ips.split("\n")
-LOCAL_IP = ips[0]
-print LOCAL_IP
+LOCAL_IP = ips[0][4:]
 
 
 macs = commands.getoutput("/sbin/ifconfig | grep -i \"HWaddr\" | awk '{print $5}'")
 macs = macs.split("\n")
-print macs[0]
+LOCAL_MAC = macs[0][]
 
 
 
